e0346a30f4023bb5c22466f3d3955dbe0b541537
Bergan Oudshoorn <bergan404@gmail.com>
2023-10-16 13:19:10 -0400

    very complicated but fun at the same time

---
+++ src/assets/js/viewport.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-undef */
+
+var resizeCover = function() {
+	var homeCover = document.getElementById( 'homecover-1' );
+	var newHeight = document.documentElement.clientHeight -
+        document.getElementById( 'navbar-1' ).clientHeight;
+	homeCover.style.height = newHeight + 'px';
+	homeCover.style.minHeight = newHeight + 'px';
+};
+
+document.addEventListener( 'DOMContentLoaded', resizeCover );
+window.addEventListener( 'resize', resizeCover );
